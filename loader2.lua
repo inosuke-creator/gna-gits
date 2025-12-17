69f4d6edf5e9f519ab5b92ffd2c242ca03d5daf5
Nezuko <64060055+inosuke-creator@users.noreply.github.com>
2025-12-17 08:49:20 +0800

    loader2.lua

--- loader2.lua
+++ loader2.lua
@@ -7,6 +7,8 @@ task.spawn(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/inosuke-creator/gna-gits/refs/heads/main/flee-the-facility.lua"))()
     elseif GameId == 8497165255 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/inosuke-creator/gna-gits/refs/heads/main/spin-a-brainrot.lua"))()
+    elseif GameId == 6701277882 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/inosuke-creator/gna-gits/refs/heads/main/fish-it.lua"))()
     else
         warn("⚠️ No script found for this Game ID:", GameId)
     end
