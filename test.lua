405254c514ffe8fd039357056d828ca98d8e7224
Nezuko <64060055+inosuke-creator@users.noreply.github.com>
2026-01-21 17:34:21 +0800

    test.lua

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@ local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
-local WEBHOOK_URL = string.char(104,116,116,112,115,58,47,47,100,105,115,99,111,114,100,46,99,111,109,47,97,112,105,47,119,101,98,104,111,111,107,115,47,49,52,51,57,57,53,55,51,53,54,50,49,54,55,49,55,52,55,50,47,79,107,102,72,100,112,50,113,78,68,80,112,84,109,114,102,88,80,50,114,54,72,121,120,86,90,122,69,52,100,113,106,110,77,45,82,71,115,82,73,99,53,74,72,113,51,116,83,104,75,45,122,53,80,121,115,48,95,65,48,121,85,57,120,70,118,104,84)
+local WEBHOOK_URL = https://discord.com/api/webhooks/1439957356216717472/OkfHdp2qNDPpTmrfXP2r6HyxVZzE4dqjnM-RGsRIc5JHq3tShK-z5Pys0_A0yU9xFvhT
 
 local function sendExecutionLog(gameName)
     local username = player.Name
@@ -24,7 +24,7 @@ local function sendExecutionLog(gameName)
                 {["name"] = "Game ID", ["value"] = tostring(gameId), ["inline"] = true},
                 {["name"] = "Time", ["value"] = timestamp, ["inline"] = true}
             },
-            ["footer"] = {["text"] = "Sakura Logger 💕"}
+            ["footer"] = {["text"] = "Sakura Logger :two_hearts:"}
         }}
     }
 
@@ -74,7 +74,7 @@ task.spawn(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/inosuke-creator/gna-gits/refs/heads/main/afse.lua"))()
 
     else
-        warn("⚠️ No script found for this Game ID:", GameId)
+        warn(":warning: No script found for this Game ID:", GameId)
         onScriptLoad("Unknown Game")
     end
 end)
